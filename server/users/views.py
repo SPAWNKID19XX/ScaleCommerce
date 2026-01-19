44ae5b56e8b5abb163a833b3d5447983a0b5e9a3
boris-isac <borisisac@gmail.com>
2026-01-19 22:38:00 +0000

    [ADD] view for registration

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import CustomUser
+from .serializers import UserRegistrationSerializer
+
 
 # Create your views here.
+class UserRegistrationView(viewsets.generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
