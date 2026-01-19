8ec3d6581da47272709dcd919e5ca35a3a3230d0
boris-isac <borisisac@gmail.com>
2026-01-19 19:16:36 +0000

    [ADD] PSQL settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7vctvzrjlj-+z$53p*^w9-$jl8ktn@1vn66jw(=svrq+gc)=ln'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+print("SECRET_KEY", SECRET_KEY)
+print(".env.user", os.environ.get('DB_USER'))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +79,17 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get('NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('PASSWORD'),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
+print("DATABASES")
+print(DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
