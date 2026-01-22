d4f8209018da6b91778ce03c9d0b84b37bcc9dc8
boris-isac <borisisac@gmail.com>
2026-01-22 01:05:02 +0000

    [ADD] models Order and OrderItem

--- server/orders/models.py
+++ server/orders/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+
+from server.products.models import Product
+
+
+class StatusOrder(models.TextChoices):
+    CREATED = "CREATED"
+    PENDING = "PENDING"
+    PAID = "PAID"
+
+
 
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    status = models.CharField(choices=StatusOrder.choices, default=StatusOrder.CREATED, max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.email} {self.status}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
